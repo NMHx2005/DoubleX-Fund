0dcfb0479b94980ea6279d29b98688f7db849f2f
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-02-23 22:19:23 +0700

    update pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,52 @@ document.addEventListener("DOMContentLoaded", function () {
         menuBtn.style.display = "block";
     });
 });
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggleButtons = document.querySelectorAll('.portfolio__toggle-button');
+
+//     toggleButtons.forEach(button => {
+//         button.addEventListener('click', function () {
+//             const icon = this.querySelector('.portfolio__icon');
+//             const details = document.querySelector('.portfolio__item-details');
+//             const bgDetails = document.querySelector('.portfolio__item');
+
+//             // Toggle class 'show' để hiển thị/ẩn nội dung chi tiết
+//             details.classList.toggle('show');
+//             bgDetails.classList.toggle('show');
+//             // Xoay mũi tên 180 độ khi hiển thị nội dung chi tiết
+//             if (details.classList.contains('show')) {
+//                 icon.style.transform = 'rotate(0deg)';
+//             } else {
+//                 icon.style.transform = 'rotate(180deg)';
+//             }
+//         });
+//     });
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButtons = document.querySelectorAll('.portfolio__toggle-button');
+
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const portfolioItem = this.closest('.portfolio__item'); // Tìm phần tử cha tương ứng
+            const details = portfolioItem.querySelector('.portfolio__item-details'); // Tìm phần chi tiết bên trong phần tử cha
+            const icon = this.querySelector('.portfolio__icon');
+
+            // Toggle class 'show' để hiển thị/ẩn nội dung chi tiết
+            details.classList.toggle('show');
+            portfolioItem.classList.toggle('show');
+
+            // Xoay mũi tên 180 độ khi hiển thị nội dung chi tiết
+            if (details.classList.contains('show')) {
+                icon.style.transform = 'rotate(0deg)';
+            } else {
+                icon.style.transform = 'rotate(180deg)';
+            }
+        });
+    });
+});
